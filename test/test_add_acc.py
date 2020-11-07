ed44cca64a0f4fda604aaf2e482e726a21bac137
Gleb Kazarkin <gkazarkin@sinergo.ru>
2020-11-07 23:55:54 +0700

    Выделен класс для работы с группами и аккаунтами (при создании новых)

--- test/test_add_acc.py
+++ test/test_add_acc.py
@@ -2,6 +2,7 @@
 from model.acc import Acc
 from fixture.application import Application
 import pytest
+from fixture.crt_acc import AccHelper
 
 # inicializator fixtyri
 @pytest.fixture()
@@ -13,10 +14,10 @@ def app(request):
 
 def test_add_acc(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_acc(
+    app.crt_acc.add_new_acc(
         Acc(firstname="Gleb", middlename="Alex", lastname="Kazarkin", nickname="gkazarkin", title="AccTitle", company="TestCompany",
             address="Yakovleva 5", home="515232", mobile="89539235812", work="367412", fax="89539234611", email="gkazarkin@test.ru",
             email2="gkazarkin@test.com", homepage="gkazarkin.com", bday="11", bmonth="April", byear="1987", aday="11", amonth="April",
             ayear="1987", phone2="515232", adress2="Yakovleva 5", notes="Test Note"))
-    app.check_add_acc()
+    app.crt_acc.check_add_acc()
     app.session.log_out()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test_group", header="test1", footer="test2"))
+    app.group.create(Group(name="test_group", header="test1", footer="test2"))
     app.session.log_out()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.log_out()
 
 
