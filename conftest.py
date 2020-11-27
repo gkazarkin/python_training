e985b0f70bb4e8abd618d18b36bfbc03857386bb
Gleb Kazarkin <gkazarkin@sinergo.ru>
2020-11-27 21:53:56 +0700

    Конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from fixture.application import Application
 import datetime
 import json
+import os.path
 
 # Предварительно запустить локальный сервер "XAMPP Control Panel"
 fixture = None
@@ -14,10 +15,12 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
+        # 1- выясняем абсолютный путь до проекта и подклеиваем к нему нужный путь
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
         # Здесь может падать, так как ищет target.json в другой папке, для этого надо указать папку проекта в Edit Configuration, Working directory
         # Или надо запускать из консоли из папки проекта, а не тестов
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target["baseUrl"])
     fixture.session.ensure_login(username=target["username"], password=target["password"])
@@ -34,7 +37,6 @@ def stop(request):
 
 def pytest_addoption(parser):  # Передаётся парсер командной строки
     parser.addoption("--browser", action="store", default="chrome")  # Доступ передаётся через объект request
-    # parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")  # Доступ передаётся через объект request
     parser.addoption("--target", action="store", default="target.json")  # Доступ передаётся через объект request
 
 
